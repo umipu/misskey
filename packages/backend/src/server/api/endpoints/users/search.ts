b4e8b86d85aa92a5be4ad411cf7a1b3569a60147
GrapeApple0 <84321396+GrapeApple0@users.noreply.github.com>
2023-08-07 11:50:16 +0000

    PGroongaに対応

--- packages/backend/src/server/api/endpoints/users/search.ts
+++ packages/backend/src/server/api/endpoints/users/search.ts
@@ -80,8 +80,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 			} else {
 				const nameQuery = this.usersRepository.createQueryBuilder('user')
 					.where(new Brackets(qb => {
-						qb.where('user.name ILIKE :query', { query: '%' + sqlLikeEscape(ps.query) + '%' });
-
+						qb.where('user.name &@~ :query', { query: '%' + sqlLikeEscape(ps.query) + '%' });
 						// Also search username if it qualifies as username
 						if (this.userEntityService.validateLocalUsername(ps.query)) {
 							qb.orWhere('user.usernameLower LIKE :username', { username: '%' + sqlLikeEscape(ps.query.toLowerCase()) + '%' });
@@ -108,7 +107,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 				if (users.length < ps.limit) {
 					const profQuery = this.userProfilesRepository.createQueryBuilder('prof')
 						.select('prof.userId')
-						.where('prof.description ILIKE :query', { query: '%' + sqlLikeEscape(ps.query) + '%' });
+						.where('prof.description &@~ :query', { query: '%' + sqlLikeEscape(ps.query) + '%' });
 
 					if (ps.origin === 'local') {
 						profQuery.andWhere('prof.userHost IS NULL');
