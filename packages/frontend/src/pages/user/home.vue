6856428c49124659cd477265b3df3c68b1e19055
GrapeApple0 <84321396+GrapeApple0@users.noreply.github.com>
2023-10-08 11:28:19 +0000

    ユーザーのプロフィールの表示を今まで通りに

--- packages/frontend/src/pages/user/home.vue
+++ packages/frontend/src/pages/user/home.vue
@@ -132,7 +132,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 					<XActivity :key="user.id" :user="user"/>
 				</template>
 				<div v-if="!disableNotes">
-					<div style="margin-bottom: 8px;">{{ i18n.ts.featured }}</div>
 					<MkNotes :class="$style.tl" :noGap="true" :pagination="pagination"/>
 				</div>
 			</div>
@@ -213,7 +212,7 @@ watch($$(moderationNote), async () => {
 });
 
 const pagination = {
-	endpoint: 'users/featured-notes' as const,
+	endpoint: 'users/notes' as const,
 	limit: 10,
 	params: computed(() => ({
 		userId: props.user.id,
