e7cb46de4433813d0a3fdfeabd7b83c9645ef3ed
GrapeApple0 <84321396+GrapeApple0@users.noreply.github.com>
2023-10-22 23:02:12 +0000

    引用リノートがリプライ欄に表示されないバグを修正

--- packages/frontend/src/components/MkNoteDetailed.vue
+++ packages/frontend/src/components/MkNoteDetailed.vue
@@ -156,11 +156,11 @@ SPDX-License-Identifier: AGPL-3.0-only
 	</div>
 	<div>
 		<div v-if="tab === 'replies'" :class="$style.tab_replies">
-			<MkPagination :pagination="repliesPagination">
-				<template #default="{ items }">
-					<MkNoteSub v-for="item in items" :key="item.id" :note="item" :class="$style.reply" :detail="true"/>
-				</template>
-			</MkPagination>
+			<div v-if="tab === 'replies'" :class="$style.tab_replies">
+			<div v-if="!repliesLoaded" style="padding: 16px">
+				<MkButton style="margin: 0 auto;" primary rounded @click="loadReplies">{{ i18n.ts.loadReplies }}</MkButton>
+			</div>
+			<MkNoteSub v-for="note in replies" :key="note.id" :note="note" :class="$style.reply" :detail="true"/>
 		</div>
 		<div v-else-if="tab === 'renotes'" :class="$style.tab_renotes">
 			<MkPagination :pagination="renotesPagination" :disableAutoLoad="true">
