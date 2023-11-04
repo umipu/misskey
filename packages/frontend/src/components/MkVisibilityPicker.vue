4d600a3c1ce9046681ac5e348515deefc0709362
GrapeApple0 <84321396+GrapeApple0@users.noreply.github.com>
2023-11-04 15:23:50 +0000

    修正忘れ

--- packages/frontend/src/components/MkVisibilityPicker.vue
+++ packages/frontend/src/components/MkVisibilityPicker.vue
@@ -51,69 +51,48 @@ SPDX-License-Identifier: AGPL-3.0-only
 </template>
 
 <script lang="ts" setup>
-import { nextTick } from 'vue';
+import { nextTick, watch } from 'vue';
 import * as Misskey from 'misskey-js';
 import MkModal from '@/components/MkModal.vue';
 import { i18n } from '@/i18n.js';
 
 const modal = $shallowRef<InstanceType<typeof MkModal>>();
-
 const props = withDefaults(defineProps<{
 	currentVisibility: typeof Misskey.noteVisibilities[number];
-	localOnly: boolean;
+	currentLocalOnly: boolean;
 	src?: HTMLElement;
 }>(), {
 });
-
 const emit = defineEmits<{
 	(ev: 'changeVisibility', v: typeof Misskey.noteVisibilities[number]): void;
+	(ev: 'changeLocalOnly', v: boolean): void;
 	(ev: 'closed'): void;
 }>();
-
 let v = $ref(props.currentVisibility);
+let localOnly = $ref(props.currentLocalOnly);
+
+watch($$(localOnly), () => {
+	emit('changeLocalOnly', localOnly);
+});
 
 function choose(visibility: typeof Misskey.noteVisibilities[number]): void {
 	v = visibility;
 	emit('changeVisibility', visibility);
 	nextTick(() => {
-		if (modal) modal.close();
+		modal.close();
 	});
 }
 </script>
 
 <style lang="scss" module>
 .root {
-	min-width: 240px;
+	width: 240px;
 	padding: 8px 0;
-
-	&.asDrawer {
-		padding: 12px 0 max(env(safe-area-inset-bottom, 0px), 12px) 0;
-		width: 100%;
-		border-radius: 24px;
-		border-bottom-right-radius: 0;
-		border-bottom-left-radius: 0;
-
-		.label {
-			pointer-events: none;
-			font-size: 12px;
-			padding-bottom: 4px;
-			opacity: 0.7;
-		}
-
-		.item {
-			font-size: 14px;
-			padding: 10px 24px;
-		}
-	}
 }
-
-.label {
-	pointer-events: none;
-	font-size: 10px;
-	padding-bottom: 4px;
-	opacity: 0.7;
+.divider {
+	margin: 8px 0;
+	border-top: solid 0.5px var(--divider);
 }
-
 .item {
 	display: flex;
 	padding: 8px 14px;
@@ -121,20 +100,21 @@ function choose(visibility: typeof Misskey.noteVisibilities[number]): void {
 	text-align: left;
 	width: 100%;
 	box-sizing: border-box;
-
 	&:hover {
 		background: rgba(0, 0, 0, 0.05);
 	}
-
 	&:active {
 		background: rgba(0, 0, 0, 0.1);
 	}
-
 	&.active {
+		color: var(--fgOnAccent);
+		background: var(--accent);
+	}
+	&.localOnly.active {
 		color: var(--accent);
+		background: inherit;
 	}
 }
-
 .icon {
 	display: flex;
 	justify-content: center;
@@ -146,20 +126,28 @@ function choose(visibility: typeof Misskey.noteVisibilities[number]): void {
 	margin-top: auto;
 	margin-bottom: auto;
 }
-
 .body {
 	flex: 1 1 auto;
 	white-space: nowrap;
 	overflow: hidden;
 	text-overflow: ellipsis;
 }
-
 .itemTitle {
 	display: block;
 	font-weight: bold;
 }
-
 .itemDescription {
 	opacity: 0.6;
+}
+.toggle {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin-left: 10px;
+	width: 16px;
+	top: 0;
+	bottom: 0;
+	margin-top: auto;
+	margin-bottom: auto;
 }
 </style>
