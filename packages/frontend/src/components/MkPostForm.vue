5e3584fd33f7461e8ef81afb4f2bb0236a062374
GrapeApple0 <84321396+GrapeApple0@users.noreply.github.com>
2023-12-09 12:56:02 +0000

    変更漏れ

--- packages/frontend/src/components/MkPostForm.vue
+++ packages/frontend/src/components/MkPostForm.vue
@@ -33,8 +33,8 @@ SPDX-License-Identifier: AGPL-3.0-only
 				</button>
 			</template>
 			<button v-click-anime v-tooltip="i18n.ts._visibility.disableFederation" class="_button" :class="[$style.headerRightItem, { [$style.danger]: localOnly }]" :disabled="channel != null || visibility === 'specified'" @click="toggleLocalOnly">
-				<span v-if="!localOnly"><i class="ti ti-rocket"></i></span>
-				<span v-else><i class="ti ti-rocket-off"></i></span>
+				<span v-if="!localOnly"><i class="ti ti-ti-world"></i></span>
+				<span v-else><i class="ti ti-world-off"></i></span>
 			</button>
 			<button v-click-anime v-tooltip="i18n.ts.reactionAcceptance" class="_button" :class="[$style.headerRightItem, { [$style.danger]: reactionAcceptance === 'likeOnly' }]" @click="toggleReactionAcceptance">
 				<span v-if="reactionAcceptance === 'likeOnly'"><i class="ti ti-heart"></i></span>
@@ -466,6 +466,7 @@ function setVisibility() {
 			}
 		},
 		changeLocalOnly: v => {
+			console.log(v);
 			localOnly.value = v;
 			if (defaultStore.state.rememberNoteVisibility) {
 				defaultStore.set('localOnly', localOnly.value);
