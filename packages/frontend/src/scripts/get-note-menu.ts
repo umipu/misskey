0e88479bd061c5fa2ad6f778a745827e86bf10e8
GrapeApple0 <84321396+GrapeApple0@users.noreply.github.com>
2023-12-13 07:52:21 +0000

    自動的にローカルのみになるのを修正

--- packages/frontend/src/scripts/get-note-menu.ts
+++ packages/frontend/src/scripts/get-note-menu.ts
@@ -500,7 +500,6 @@ export function getRenoteMenu(props: {
 				}
 
 				const configuredVisibility = defaultStore.state.rememberNoteVisibility ? defaultStore.state.visibility : defaultStore.state.defaultNoteVisibility;
-				const localOnly = defaultStore.state.rememberNoteVisibility ? defaultStore.state.localOnly : defaultStore.state.defaultNoteLocalOnly;
 
 				let visibility: Visibility = appearNote.visibility as Visibility;
 				visibility = smallerVisibility(visibility, configuredVisibility);
@@ -510,7 +509,6 @@ export function getRenoteMenu(props: {
 
 				if (!props.mock) {
 					os.api('notes/create', {
-						localOnly,
 						visibility,
 						renoteId: appearNote.id,
 					}).then(() => {
